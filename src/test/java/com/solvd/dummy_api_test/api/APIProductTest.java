026f86e5f1e8215ff7d884f4edce6e80f0262a91
Nazareno Bucciarelli <bnazareno03@gmail.com>
2024-02-05 17:54:47 -0300

    first commit

---
+++ src/test/java/com/solvd/dummy_api_test/api/APIProductTest.java
@@ -0,0 +1,49 @@
+package com.solvd.dummy_api_test.api;
+
+import com.solvd.dummy_api_test.api.products.DeleteProductById;
+import com.solvd.dummy_api_test.api.products.GetProductById;
+import com.solvd.dummy_api_test.api.products.PostProduct;
+import com.solvd.dummy_api_test.api.products.PutProductById;
+import com.zebrunner.carina.core.IAbstractTest;
+import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
+import org.testng.annotations.Test;
+
+public class APIProductTest implements IAbstractTest {
+
+    @Test
+    @MethodOwner(owner = "nazareno")
+    public void getProductByIdTest() {
+        GetProductById getProductById = new GetProductById();
+        getProductById.setId(20L);
+        getProductById.callAPIExpectSuccess();
+        getProductById.validateResponseAgainstSchema("api/products/_get/rs.schema");
+    }
+
+    @Test
+    @MethodOwner(owner = "nazareno")
+    public void postProductTest() {
+        PostProduct postProduct = new PostProduct();
+        postProduct.setProperties("api/products/product.properties");
+        postProduct.callAPIExpectSuccess();
+        postProduct.validateResponseAgainstSchema("api/products/_post/rs.schema");
+    }
+
+    @Test
+    @MethodOwner(owner = "nazareno")
+    public void deleteProductTest() {
+        DeleteProductById deleteProductById = new DeleteProductById();
+        deleteProductById.setId(90L);
+        deleteProductById.callAPIExpectSuccess();
+        deleteProductById.validateResponseAgainstSchema("api/products/_delete/rs.schema");
+    }
+
+    @Test
+    @MethodOwner(owner = "nazareno")
+    public void putProductByIdTest() {
+        PutProductById putProductById = new PutProductById();
+        putProductById.setId(2L);
+        putProductById.setProperties("api/products/product.properties");
+        putProductById.callAPIExpectSuccess();
+        putProductById.validateResponseAgainstSchema("api/products/_put/rs.schema");
+    }
+}
---
+++ src/test/java/com/solvd/dummy_api_test/api/APIUserTest.java
@@ -0,0 +1,48 @@
+package com.solvd.dummy_api_test.api;
+
+import com.solvd.dummy_api_test.api.users.DeleteUserById;
+import com.solvd.dummy_api_test.api.users.GetAllUser;
+import com.solvd.dummy_api_test.api.users.PostUser;
+import com.solvd.dummy_api_test.api.users.PutUserById;
+import com.zebrunner.carina.core.IAbstractTest;
+import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
+import org.testng.annotations.Test;
+
+public class APIUserTest implements IAbstractTest {
+
+    @Test
+    @MethodOwner(owner = "nazareno")
+    public void getAllUserTest() {
+        GetAllUser getAllUser = new GetAllUser();
+        getAllUser.callAPIExpectSuccess();
+        getAllUser.validateResponseAgainstSchema("api/users/_get/rs.schema");
+    }
+
+    @Test
+    @MethodOwner(owner = "nazareno")
+    public void postUserTest() {
+        PostUser postUser = new PostUser();
+        postUser.setProperties("api/users/user.properties");
+        postUser.callAPIExpectSuccess();
+        postUser.validateResponseAgainstSchema("api/users/_post/rs.schema");
+    }
+
+    @Test
+    @MethodOwner(owner = "nazareno")
+    public void putUserTest() {
+        PutUserById putUserById = new PutUserById();
+        putUserById.setId(10L);
+        putUserById.setProperties("api/users/user.properties");
+        putUserById.callAPIExpectSuccess();
+        putUserById.validateResponseAgainstSchema("api/users/_put/rs.schema");
+    }
+
+    @Test
+    @MethodOwner(owner = "nazareno")
+    public void deleteUserTest() {
+        DeleteUserById deleteUserById = new DeleteUserById();
+        deleteUserById.setId(5L);
+        deleteUserById.callAPIExpectSuccess();
+        deleteUserById.validateResponseAgainstSchema("api/users/_delete/rs.schema");
+    }
+}
